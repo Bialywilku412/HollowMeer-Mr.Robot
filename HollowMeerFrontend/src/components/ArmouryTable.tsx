9f4551cf4d1d220a0a5883f68f4633a8f90c0d03
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-23 10:33:08 +0200

    Armoury table component

---
+++ HollowMeerFrontend/src/components/ArmouryTable.tsx
@@ -0,0 +1,31 @@
+import type { Weapon } from "../features/ArmouryPage";
+import "../Armoury.css"
+
+type WeaponListProps = {
+    weapons: Weapon[];
+}
+
+function ArmouryTable({ weapons }: WeaponListProps) {
+    return (
+        <table className="armoury">
+            <tr className="header">
+                <th>name</th>
+                <th>type</th>
+                <th>condition</th>
+                <th>dangerous</th>
+                <th>note</th>
+            </tr>
+            {weapons.map((weapon) => (
+                <tr className="row">
+                <td>{weapon.name}</td>
+                <td>{weapon.type}</td>
+                <td>{weapon.condition}</td>
+                <td>{weapon.dangerous ? "yes" : "no"}</td>
+                <td>{weapon.note}</td>
+            </tr>
+            ))}
+        </table>
+    );
+}
+
+export default ArmouryTable;

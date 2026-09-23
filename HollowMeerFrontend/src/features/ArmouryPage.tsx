fef4b05812e5455431d2c5f89adce44ff9a9b4af
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-23 10:10:39 +0200

    homepage and armoury placeholder

---
+++ HollowMeerFrontend/src/features/ArmouryPage.tsx
@@ -0,0 +1,9 @@
+function Armoury() {
+  return (
+    <>
+      <h1>Armoury</h1>
+      <p>aaaaaa.</p>
+    </>
+  );
+}
+export default Armoury;
---
+++ HollowMeerFrontend/src/pages/Homepage.tsx
@@ -0,0 +1,8 @@
+export function HomePage() {
+  return (
+    <>
+      <h1>The Bakery</h1>
+      <p>Pick a page from the menu.</p>
+    </>
+  );
+}

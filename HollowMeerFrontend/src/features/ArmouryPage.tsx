55f38e240451e0531d71497afe8d6b96bffdd83b
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-23 10:33:41 +0200

    Armoury page with a  simple table of weapons

--- HollowMeerFrontend/src/features/ArmouryPage.tsx
+++ HollowMeerFrontend/src/features/ArmouryPage.tsx
@@ -1,9 +1,25 @@
+import ArmouryTable from "../components/ArmouryTable";
+export type Weapon = {
+    name: string;
+    type: string;
+    condition: string
+    dangerous: boolean;
+    note: string;
+
+}
 function Armoury() {
-  return (
-    <>
-      <h1>Armoury</h1>
-      <p>aaaaaa.</p>
-    </>
-  );
+    const weapons: Weapon[] = [
+        { name: "excalibur", type: "sword", condition: "bad", dangerous: true, note: "aaaa"},
+        { name: "excalibur", type: "sword", condition: "bad", dangerous: true, note: "aaaa"},
+        { name: "excalibur", type: "sword", condition: "bad", dangerous: true, note: "aaaa"},
+        { name: "excalibur", type: "sword", condition: "bad", dangerous: true, note: "aaaa"},
+    ];
+
+    return (
+        <>
+            <ArmouryTable
+                weapons={weapons}/>
+        </>
+    );
 }
 export default Armoury;

86cf7ecbc8dec81b77691d925c164b39f1fc4745
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-26 12:01:33 +0200

    Route to ArmouryWeaponDetailPage

--- HollowMeerFrontend/src/App.tsx
+++ HollowMeerFrontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Navbar from "./components/Navbar";
 import { HomePage } from "./pages/Homepage";
 import Armoury from "./features/armoury/ArmouryPage";
+import ArmouryWeaponDetailPage from "./features/armoury/ArmouryWeaponDetailPage";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
           <Route index element={<HomePage />} />
           <Route path="armoury" element={<Armoury />} />
+          <Route path="armoury/:weaponId" element={<ArmouryWeaponDetailPage />} />
       </Routes>
     </>
   );
